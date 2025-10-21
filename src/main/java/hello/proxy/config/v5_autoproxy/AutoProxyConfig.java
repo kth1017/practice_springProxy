9b7cd3df4a84971322b71b57edf5ba43ebbe99d8
KTH <rnjsxo1017@gmail.com>
2025-10-21 22:10:33 +0900

    ~ aspect aop 1

---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,28 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+    // 빈 후처리기 코드 제거
+
+    @Bean
+    public Advisor advisor1(LogTrace logTrace) {
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        //advisor = pointcut + advice
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
